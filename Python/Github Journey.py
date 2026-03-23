e4564cdcd2fb3ff9f96bbc9a19977777f4b33e15
mannu-3935 <mannudongre3935@gmail.com>
2026-03-23 15:02:29 +0530

    Coding Practice

---
+++ Python/Github Journey.py
@@ -0,0 +1,8 @@
+# git clone https://github.com/mannu-3935/DSA_Practice.git      -HTTPS key
+# git clone git@github.com:mannu-3935/DSA_Practice.git          -SSH key
+# gh repo clone mannu-3935/DSA_Practice                         -GitHub CLI     owner-name/repo
+
+print("GitHub Repository Cloning Commands:")
+print("# git clone https://github.com/mannu-3935/DSA_Practice.git      -HTTPS key")
+print("# git clone git@github.com:mannu-3935/DSA_Practice.git          -SSH key")
+print("# gh repo clone mannu-3935/DSA_Practice                         -GitHub CLI     owner-name/repo")
