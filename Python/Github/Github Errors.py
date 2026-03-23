1cb9c7383fb267e005c3210c968c2a1d2e757786
mannu-3935 <mannudongre3935@gmail.com>
2026-03-23 15:12:34 +0530

    Coding Practice

---
+++ Python/Github/Github Errors.py
@@ -0,0 +1,12 @@
+'''
+PS C:\Users\manisha\OneDrive\Desktop\DSA Practice\DSA_Practice> git push                                                   
+remote: Permission to mannu-3935/DSA_Practice.git denied to anshu-cloud.
+fatal: unable to access 'https://github.com/mannu-3935/DSA_Practice.git/': The requested URL returned error: 403
+
+git config user.name "mannu-3935"  
+git config user.email "mannudongre3935@gmail.com"  
+
+git remote set-url origin git@github.com:mannu-3935/DSA_Practice.git
+git push
+
+'''
